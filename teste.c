f158961566089cfb9def7b3993b937550ac09000
Matheus Casa Nova da Luz <mcnl@cin.ufpe.br>
2018-04-10 16:31:51 -0300

    Funções Par() e Modd() adicionadas

---
+++ teste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+bool par(int a){
+	if(a%2==0){
+		return true;
+	}
+	return false;
+}
+
+int modd(int a){
+	if(a<0){
+		a=a*-1;
+	}
+	return a;
+}
+
+int main(){
+
+
+return 0;
+}
